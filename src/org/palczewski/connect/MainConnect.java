435156b97e1ccbe9929c79e596c4f5f83e5b1c7a
Patrick Palczewski <psykiatris@gmail.com>
2019-03-30 08:41:24 -0700

    Initial commit

---
+++ src/org/palczewski/connect/MainConnect.java
@@ -0,0 +1,12 @@
+package org.palczewski.connect;
+
+import java.sql.Connection;
+
+/*
+This class will provide the main connection to mySQL. Can be used by
+other applications.
+ */
+public class MainConnect {
+
+    Connection conn;
+}
