bf15526440f3d78ef4710629f7c6dd64fce1f446
Patrick Palczewski <psykiatris@gmail.com>
2019-04-06 10:29:01 -0700

    Worked on updateRecord().

--- src/org/palczewski/edit/TableMachine.java
+++ src/org/palczewski/edit/TableMachine.java
@@ -5,6 +5,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /*
 This class contains methods to be used with tables (creation, deletion,
@@ -14,10 +16,11 @@ Methods will be built with predefined use (as general as possible), but
 can be overridden by programs utilizing this library.
  */
 public class TableMachine {
+    public static final String NOTHING_TO_SHOW = "Nothing to show";
     /*
-    Other apps can pass their connection objects to use methods in this
-    class. Same idea as DatabaseMachine.
-     */
+        Other apps can pass their connection objects to use methods in this
+        class. Same idea as DatabaseMachine.
+         */
     Connection conn;
     Statement stmt;
 
@@ -40,12 +43,13 @@ public class TableMachine {
             try {
                 stmt = conn.createStatement();
                 String table =
-                        MessageFormat.format("CREATE TABLE IF NOT EXISTS {0} (date DATE PRIMARY KEY, lname CHAR(15), fname CHAR(15));", tName);
+                        MessageFormat.format("CREATE TABLE IF NOT EXISTS {0} (date TIMESTAMP PRIMARY KEY, lname CHAR(15), fname CHAR(15));", tName);
                 stmt.executeUpdate(table);
                 stmt.close();
+                System.out.println(MessageFormat.format("Successfully created {0} table.", tName));
 
             } catch (SQLException e) {
-                System.out.println("SQL error in createTable(): " + e.getMessage());
+                System.out.println(MessageFormat.format("SQL error in createTable(): {0}", e.getMessage()));
             }
         } else {
             System.out.println(DatabaseMachine.NO_CONNECTION);
@@ -62,7 +66,7 @@ public class TableMachine {
                 try(ResultSet rs = stmt.executeQuery(show)) {
                     while(rs.next()) {
                         if(rs.getString(1) == null) {
-                            System.out.println("Nothing to show");
+                            System.out.println(NOTHING_TO_SHOW);
                         } else {
                             System.out.println(rs.getString(1));
                         }
@@ -73,6 +77,77 @@ public class TableMachine {
             } catch (SQLException e) {
                 System.out.println(MessageFormat.format("SQL error in viewTables(): {0}", e.getMessage()));
             }
+        }
+    }
+
+    public void insertRecord() {
+        /*
+        Other apps MUST override this method. This will insert a default
+         record into the table defined in the template createTable().
+         */
+        if(conn != null) {
+            try {
+                stmt = conn.createStatement();
+                String data = MessageFormat.format("INSERT INTO history (date, lname, fname) VALUES (\"{0}\", \"Holguin\", \"Jeffrey\")",
+                        LocalDateTime.now());
+                stmt.executeUpdate(data);
+                stmt.close();
+
+            } catch (SQLException e) {
+                System.out.println(MessageFormat.format("SQL error in insertRecord(): {0}", e.getMessage()));
+            }
+        } else {
+            System.out.println(DatabaseMachine.NO_CONNECTION);
+        }
+    }
+
+    public void viewRecords(String tName) {
+        /*
+        Will display records from table
+         */
+        if(conn != null) {
+            try {
+                stmt = conn.createStatement();
+                String qry = MessageFormat.format("SELECT * FROM {0}", tName);
+                try(ResultSet rs = stmt.executeQuery(qry)) {
+                    System.out.println("Records Display:");
+                    while(rs.next()) {
+                        if(rs.getString(1) == null) {
+                            System.out.println(NOTHING_TO_SHOW);
+                        } else {
+                            System.out.println(MessageFormat.format("{0}\t{1}\t{2}", rs.getTimestamp(1),
+                                    rs.getString(2)
+                                    , rs.getString(3)));
+                        }
+                    }
+                }
+            } catch (SQLException e) {
+                System.out.println(MessageFormat.format("SQL error in viewRecords(): {0}", e.getMessage()));
+            }
+        } else {
+            System.out.println(DatabaseMachine.NO_CONNECTION);
+        }
+    }
+
+    public void updateRecord(String tName) {
+        /*
+        This will update one row in the default template table. other
+        apps should override this method for their own tables, etc.
+         */
+        if(conn != null) {
+            try {
+                stmt = conn.createStatement();
+                String update = MessageFormat.format("UPDATE {0} date = TIMESTAMP", tName);
+
+                stmt.executeUpdate(update);
+                stmt.close();
+                System.out.println("Updated record");
+
+            } catch (SQLException e) {
+                System.out.println(MessageFormat.format("SQL error in updateRecord(): {0}", e.getMessage()));
+            }
+        } else {
+            System.out.println(DatabaseMachine.NO_CONNECTION);
         }
     }
 
