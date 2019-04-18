12a4b0e111ff39ecffd6bd36507f8234fd9bef85
Patrick Palczewski <psykiatris@gmail.com>
2019-04-17 18:03:56 -0700

    Did a DataSource example. Will incorporate into MainConnect and change name to SQLConnect.

---
+++ src/org/palczewski/connect/DataSourceTest.java
@@ -0,0 +1,55 @@
+package org.palczewski.connect;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataSourceTest {
+
+    public static void main(String[] args) {
+
+        testDataSource("mysql");
+        System.out.println("*************");
+
+    }
+
+    private static void testDataSource(String dbType) {
+
+        DataSource ds = null;
+        if("mysql".equals(dbType)) {
+            ds = MyDataSourceFactory.getMySQLDataSource();
+        } else {
+            System.out.println("No dbType known");
+        }
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = ds.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("select empId, name from Employee");
+            while(rs.next()) {
+                System.out.println("Employee ID: " + rs.getInt("empId") + ", Name: " + rs.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("SQL error in testDatabase(): " + e.getMessage());
+        } finally {
+            try {
+                if(rs !=  null) rs.close();
+                if(stmt != null) stmt.close();
+                if(conn != null) conn.close();
+            } catch (SQLException e) {
+                System.out.println("SQL error in closing block");
+            }
+        }
+
+    }
+
+
+}
---
+++ src/org/palczewski/connect/MyDataSourceFactory.java
@@ -0,0 +1,36 @@
+package org.palczewski.connect;
+/*
+A DataSource Factory class to handle connections
+ */
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MyDataSourceFactory {
+
+    public static DataSource getMySQLDataSource() {
+
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_USERNAME "));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO exception in MyDataSourceFactory(): " + e.getMessage());
+        }
+        return mysqlDS;
+    }
+}
