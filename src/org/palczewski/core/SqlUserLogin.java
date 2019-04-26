5da8817d7dc45c5ee5840620ddc70f0e7a8b69da
Patrick Palczewski <psykiatris@gmail.com>
2019-04-26 07:00:50 -0700

    Gave up on preparedQuery implementation until later.

--- src/org/palczewski/core/SqlUserLogin.java
+++ src/org/palczewski/core/SqlUserLogin.java
@@ -2,10 +2,12 @@ package org.palczewski.core;
 
 import java.util.logging.Logger;
 
-public final class SqlUserLogin {
+public class SqlUserLogin {
 
     private static final Logger logger =
             Logger.getLogger(SqlUserLogin.class.getName());
+
+
 
 
 
