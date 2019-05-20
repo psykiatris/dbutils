59fa7cabffe5b15eaa01ec93173adf4aa4b49610
Patrick Palczewski <psykiatris@gmail.com>
2019-05-20 15:48:31 -0700

    Finished class. Now will work on what's missing, such as SeessionWrappers, etc.

--- src/org/palczewski/proposed/PoolManager.java
+++ src/org/palczewski/proposed/PoolManager.java
@@ -1,24 +1,28 @@
 package org.palczewski.proposed;
 
 import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import com.mysql.cj.jdbc.MysqlPooledConnection;
 
 
+import javax.sql.ConnectionEvent;
 import javax.sql.ConnectionEventListener;
 import javax.sql.PooledConnection;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.*;
 import java.util.logging.Logger;
 
-
-
-
 /*
-Proposed class to managed pooled connections
+Proposed class to managed pooled connections.
+
+Based on a class developed by Jakob Jenkov.
+
+
  */
-public class PoolManager implements MysqlConnectionPoolDataSource,
+public class PoolManager extends MysqlConnectionPoolDataSource implements
         ConnectionEventListener {
 
 
@@ -334,10 +338,335 @@ public class PoolManager implements MysqlConnectionPoolDataSource,
         return this.connectionInUse.size() + this.connectionsInactive.size();
     }
 
-    private Connection wrapConnectionAndMarkAsInUse() {
+    private Connection wrapConnectionAndMarkAsInUse(MysqlPooledConnection pooledConnection) throws SQLException {
+
+        pooledConnection = assureValidConnection(pooledConnection);
+
+        Connection conn = pooledConnection.getConnection();
+
+        if(doResetAutoCommit) {
+            conn.setAutoCommit(isAutoCommit);
+
+        }
+
+        if(doResetReadOnly) {
+            conn.setReadOnly(isReadOnlye);
+        }
+
+        if(doResetTrasactionIsolation) {
+            conn.setTransactionIsolation(transactionIsolation);
+        }
+
+        if(doResetCatalog) {
+            conn.setCatalog(catalog);
+        }
+
+        if(validationQuery != null) {
+
+            java.sql.ResultSet rs = null;
+
+            try {
+
+                rs = conn.createStatement().executeQuery(validationQuery);
+
+                if(!rs.next()) {
+                    throw new SQLException(" 0 rows returned");
+                }
+            } catch (SQLException e) {
+                closePhysically(pooledConnection, "Closing " +
+                        "non0validateing pooled connection");
+                throw new SQLException("Validation query failed. " + e.getMessage());
+            } finally {
+                if(rs != null) {
+                    rs.close();
+                }
+            }
+        }
+        this.connectionInUse.add(pooledConnection);
+
+        SessionConnectionWrapper sessionWrapper =
+                new SessionConnectionWrapper(pooledConnection.getConnection());
+
+        this.sessionConnectionWrappers.put(pooledConnection,
+                sessionWrapper);
+
+        return sessionWrapper;
+
 
     }
 
+    private MysqlPooledConnection assureValidConnection(MysqlPooledConnection pooledConnection) throws SQLException {
+
+        if(isInvalid(pooledConnection)) {
+            closePhysically(pooledConnection, "Closing invalid pooled " +
+                    "connection");
+
+            return this.connectionPoolDatasource.getConnection();
+        }
+        return pooledConnection;
+    }
+
+    private boolean isInvalid(MysqlPooledConnection pooledConnection) {
+        try {
+            pooledConnection.getConnection().isValid() || pooledConnection.getConnection().isClosed();
+        } catch (SQLException e) {
+            logInfo("Error calling pooledConnection. Connection will be " +
+                    "removed from pool.", e);
+        }
+        return false;
+    }
+
+    private boolean isSessionTimedOut(long now,
+                                      ConnectionSessionWrapper sessionWrapper, long sessionTimeoutMills) {
+        return now - sessionWrapper.getLatestActivityTime() >= sessionTimeoutMills;
+    }
+
+    private boolean poolHasSpaceForNewConnections() {
+        return this.maxPoolSize > size();1
+    }
+
+    public synchronized void connectionClosed(ConnectionEvent event) {
+
+        MysqlPooledConnection connection =
+                (MysqlPooledConnection) event.getSource();
+
+        this.connectionInUse.remove(connection);
+        this.sessionConnectionWrappers.remove(connection);
+
+        if(!isPoolClosed) {
+            enqueue(connection);
+            logInfo("Connection returned to pool.");
+
+        } else {
+            closePhysically(connection, "closing returned connection");
+            logInfo("Connection returned to pool was closed because pool" +
+                    " is closed.");
+            this.notifyAll();
+        }
+    }
+
+    public synchronized void connectionErrorOccurred(ConnectionEvent event) {
+
+        MysqlPooledConnection connection =
+                (MysqlPooledConnection) event.getSource();
+        connection.removeStatementEventListener(this);
+        this.connectionInUse.remove(connection);
+        this.sessionConnectionWrappers.remove(connection);
+        logInfo("Fatal exception occurred on pooled connection. " +
+                "Connection removed from pool.");
+        logInfo(event.getSQLException());
+        closePhysically(connection, "closing invalid, removed connection" +
+                ".");
+        this.notifyAll();
+    }
+
+    public synchronized void close() {
+
+        this.isPoolClosed = true;
+
+        while(this.connectionsInactive.size() > 0) {
+            MysqlPooledConnection connection = dequeueFirstIfAny();
+
+            if(connection != null) {
+                closePhysically(connection, "closing inactive connection" +
+                        " when connection pool was closed.");
+            }
+        }
+    }
+
+    public synchronized void closeAndWait() throws InterruptedException {
+
+        close();
+
+        while(size() > 0) {
+            this.wait();
+        }
+    }
+
+    public synchronized void closeimmediately() {
+
+        close();
+
+        Iterator iterator = connectionInUse.iterator();
+
+        while(iterator.hasNext()) {
+            MysqlPooledConnection connection =
+                    (MysqlPooledConnection) iterator.next();
+            SessionConnectionWrapper sessionWrapper =
+                    (SessionConnectionWrapper) this.sessionConnectionWrappers.get(connection);
+            closeSessionWrapper(sessionWrapper, "Error closing session " +
+                    "wrapper. Connection pool was shutdown immediately");
+        }
+    }
+
+    private void closePhysically(MysqlPooledConnection source,
+                                 String logText) {
+        try {
+            source.close();
+
+        } catch (SQLException e) {
+            logInfo("Error " + logText, e);
+        }
+    }
+
+    private void logInfo(String message) {
+
+        connectionPoolDatasource.logInfo(message);
+    }
+
+    private void logInfo(Throwable t) {
+
+        connectionPoolDatasource.logInfo(t);
+    }
+
+    private void logInfo(String message, Throwable t) {
+
+        connectionPoolDatasource.logInfo(message, t);
+    }
+
+    public void setDefaultAutoCommnit(boolean defaultAutoCommit) {
+        isAutoCommit = defaultAutoCommit;
+        doResetAutoCommit = true;
+    }
+
+    public void setDefaultReadOnly(boolean defaultReadOnly) {
+
+        isReadOnlye = defaultReadOnly;
+        doResetReadOnly = true;
+    }
+
+    public void setDefaultTransactionIsolation(int defaultTransactionIsolation) {
+
+        transactionIsolation = defaultTransactionIsolation;
+        doResetTransactionIsolation = true;
+    }
+
+    public void setDefaultCatalog(String defaultCatalog) {
+
+        catalog = defaultCatalog;
+        doResetCatalog = true;
+    }
+
+    public boolean getDefaultAutoCommit() {
+        doResetAutoCommit = true;
+
+        return isAutoCommit;
+    }
+
+    public String getDefaultCatalog() {
+        doResetCatalog = true;
+
+        return catalog;
+    }
+
+    public int getDefaultTransactionIsolatio() {
+        doResetTransactionIsolation = true;
+        return transactionIsolation;
+    }
+
+    public boolean getDefaultReadOnly() {
+        doResetReadOnly = true;
+        return isReadOnlye;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        if(driverClassName.equals(MysqlDataSource.mysqlDriver) {
+            return;
+        }
+
+        throw new RuntimeException("This class only supports " + MysqlDataSource.mysqlDriver);
+    }
+
+    public String getDriverClassName() {
+        return MysqlDataSource.mysqlDriver.toString();
+    }
+
+    public void setInitialSize(int initialSize) {
+        this.initialSize = initialSize;
+    }
+
+    public int getinitialPoolSize() {
+        return getInitialSize();
+    }
+
+    public void setInitialPoolSize(int initialSize) {
+
+        setInitialSize(initialSize);
+
+    }
+
+    public int getInitialSize() {
+        return initialSize;
+    }
+
+    public int getnumActive() {
+        return connectionInUse.size();
+    }
+
+    public void setUsername(String username) {
+        setUser(username);
+    }
+
+    public String getusername() {
+        return getUser();
+    }
+
+    public void setMaxActive(int maxActive) {
+        setMaxPoolSize(maxActive);
+    }
+
+    public int getMaxActive() {
+        return getMaxPoolSize();
+    }
+
+    public void setValidationQuery(String validationQuery) {
+
+        this.validationQuery = validationQuery;
+
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
+    }
+
+    public void addConnectionProperty(String name, String value) {
+        this.connectionPoolDatasource.setConnectionProperty(name, value);
+
+
+    }
+
+    public void removeConnectionProperty(String name) {
+
+        this.connectionPoolDatasource.removeConnectionProperty(name);
+    }
+
+    public String toString() throws RuntimeException {
+
+        int timeout = 0;
+
+        try {
+            timeout = getLoginTimeout();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to retrieve the Login " +
+                    "Timeout value.")
+        }
+
+        StringBuffer sb =
+                new StringBuffer(PoolManager.class.getName() + " " +
+                        "instance\n   User: " + getUsername()
+                + "\n   Url: " + getUrl()
+                + "\n   Login Timeout: " + timeout
+                + "\n   Num. ACTIVE: " + getnumActive()
+                + "\n   Num. IDLE: " + getNumIdle());
+
+
+
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        throw new SQLFeatureNotSupportedException();
+    }
 
 
 
