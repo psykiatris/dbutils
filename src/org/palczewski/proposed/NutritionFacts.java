d5d446ff20dea70590182cd66f89bdc995429428
Patrick Palczewski <psykiatris@gmail.com>
2019-06-14 18:57:28 -0700

    Worked on a builder. Will likely move it to DiabPro as the code is useful.

---
+++ src/org/palczewski/proposed/NutritionFacts.java
@@ -0,0 +1,75 @@
+package org.palczewski.proposed;
+/*
+Learning to create a builder for the class
+ */
+public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrates;
+
+    public static class Builder {
+
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional params with default values
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrates = 0;
+
+        public Builder(int servingSize, int servings) {
+
+            this.servingSize = servingSize;
+            this.servings = servings;
+
+        }
+
+        // Optional fields set
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+        public Builder carbohydrates(int val) {
+            carbohydrates = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrates = builder.carbohydrates;
+
+    }
+
+    public static void main(String[] args) {
+
+        NutritionFacts cocaCola =
+                new Builder(240, 8).calories(100).sodium(68).carbohydrates(27).build();
+        System.out.println(cocaCola);
+        System.out.println("Fat: " + cocaCola.fat);
+    }
+
+}
